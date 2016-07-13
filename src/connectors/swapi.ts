78e6d2046dd41450e89f333eb6324b942fec7ab1
Nick Nance <nick.nance@creditkarma.com>
2016-07-13 06:37:17 -0700

    full support for offset and limit

--- src/connectors/swapi.ts
+++ src/connectors/swapi.ts
@@ -18,21 +18,35 @@ export default class SWAPIConnector {
 
   public fetchPage(resource: string, offset?: number, limit?: number) {
     let results = []
+    let index = 0
     const size = limit || 0
 
     function pagination(pageURL: string) {
       return new Promise<any>((resolve, reject) => {
         this.fetch(pageURL).then((data) => {
-          if (size > 0 && size - results.length - data.results.length < 0) {
-            results = results.concat(data.results.slice(0, size - results.length))
+          // used for debugging
+          // console.log(`first: ${size} size: ${results.length} offset: ${offset} index: ${index} ${pageURL}`)
+          if (offset && results.length === 0) {
+            if (index + data.results.length > offset) {
+              results = data.results.slice(offset - index)
+            }
+            if (data.next) {
+              index = index + data.results.length
+              pagination.call(this, data.next).then(resolve)
+            } else {
+              resolve(results)
+            }
           } else {
-            results = results.concat(data.results)
-          }
-          console.log(`first: ${size} size: ${results.length} ${pageURL}`)
-          if (data.next && (size === 0 || size - results.length > 0)) {
-            pagination.call(this, data.next).then(resolve)
-          } else {
-            resolve(results)
+            if (size > 0 && size - results.length - data.results.length < 0) {
+              results = results.concat(data.results.slice(0, size - results.length))
+            } else {
+              results = results.concat(data.results)
+            }
+            if (data.next && (size === 0 || size - results.length > 0)) {
+              pagination.call(this, data.next).then(resolve)
+            } else {
+              resolve(results)
+            }
           }
         })
       })
