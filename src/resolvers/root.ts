06e1930dd22c501329af60eb639256401d0f5746
Nick Nance <nick.nance@creditkarma.com>
2016-07-14 15:28:03 -0700

    update resolvers to be cached

--- src/resolvers/root.ts
+++ src/resolvers/root.ts
@@ -1,21 +1,31 @@
-function person(_, params, context) {
-  return context.people.getPeople(params.id, params.personID)
-}
+const allFilms = (_, params, context) => context.film.getFilms(params.offset, params.limit
+const film = (_, params, context) => context.film.getFilm(params.id, params.filmID)
+const person = (_, params, context) => context.people.getPeople(params.id, params.personID)
+const allPeople = (_, params, context) => context.people.getPeoples(params.offset, params.limit)
+const allPlanets = (_, params, context) => context.planet.getPlanets(params.offset, params.limit)
+const planet = (_, params, context) => context.planet.getPlanet(params.id, params.planetID)
+const allSpecies = (_, params, context) => context.species.getAllSpecies(params.offset, params.limit)
+const species = (_, params, context) => context.species.getSpecies(params.id, params.speciesID)
+const allStarships = (_, params, context) => context.starship.getStarships(params.offset, params.limit)
+const starship = (_, params, context) => context.species.getStarship(params.id, params.starshipID)
+const allVehicles = (_, params, context) => context.vehicle.getVehicles(params.offset, params.limit)
+const vehicle = (_, params, context) => context.vehicle.getVehicle(params.id, params.vehicleID)
+const node = (id: number) => ({})
 
 export default {
   RootQuery: {
-    allFilms: (_, params, context) => context.film.getFilms(params.offset, params.limit),
-    film: (_, params, context) => context.film.getFilm(params.id, params.filmID),
-    allPeople: (_, params, context) => context.people.getPeoples(params.offset, params.limit),
+    allFilms,
+    film,
+    allPeople,
     person,
-    allPlanets: (_, params, context) => context.planet.getPlanets(params.offset, params.limit),
-    planet: (_, params, context) => context.planet.getPlanet(params.id, params.planetID),
-    allSpecies: (_, params, context) => context.species.getAllSpecies(params.offset, params.limit),
-    species: (_, params, context) => context.species.getSpecies(params.id, params.speciesID),
-    allStarships: (_, params, context) => context.starship.getStarships(params.offset, params.limit),
-    starship: (_, params, context) => context.species.getStarship(params.id, params.starshipID),
-    allVehicles: (_, params, context) => context.vehicle.getVehicles(params.offset, params.limit),
-    vehicle: (_, params, context) => context.vehicle.getVehicle(params.id, params.vehicleID),
-    node: (id: number) => ({}),
+    allPlanets,
+    planet,
+    allSpecies,
+    species,
+    allStarships,
+    starship,
+    allVehicles,
+    vehicle,
+    node,
   },
 }
--- src/resolvers/vehicle.ts
+++ src/resolvers/vehicle.ts
@@ -1,11 +1,19 @@
+const id = (vehicle) => vehicle.url
+const costInCredits = (vehicle) => vehicle.cost_in_credits
+const maxAtmospheringSpeed = (vehicle) => vehicle.max_atmosphering_speed
+const cargoCapacity = (vehicle) => vehicle.cargo_capacity
+const vehicleClass = (vehicle) => vehicle.vehicle_class
+const pilots = (vehicle, _, context) => context.people.getConnections(vehicle.pilots)
+const films = (vehicle, _, context) => context.film.getConnections(vehicle.films)
+
 export default {
   Vehicle: {
-    id: (vehicle) => vehicle.url,
-    costInCredits: (vehicle) => vehicle.cost_in_credits,
-    maxAtmospheringSpeed: (vehicle) => vehicle.max_atmosphering_speed,
-    cargoCapacity: (vehicle) => vehicle.cargo_capacity,
-    vehicleClass: (vehicle) => vehicle.vehicle_class,
-    pilots: (vehicle, _, context) => context.people.getConnections(vehicle.pilots),
-    films: (vehicle, _, context) => context.film.getConnections(vehicle.films),
+    id,
+    costInCredits,
+    maxAtmospheringSpeed,
+    cargoCapacity,
+    vehicleClass,
+    pilots,
+    films,
   },
 }
