bc04a1d7dff08e69fb482f9f6b312afa9b59e341
nnance <nance.nick@gmail.com>
2016-07-18 05:53:03 -0700

    Revert "update resolvers to be cached"
    This reverts commit 06e1930dd22c501329af60eb639256401d0f5746.
    
    # Conflicts:
    #	src/resolvers/root.ts

--- src/resolvers/planet.ts
+++ src/resolvers/planet.ts
@@ -1,17 +1,10 @@
-const id = (planet) => planet.url
-const rotationPeriod = (planet) => planet.rotation_period
-const orbitalPeriod = (planet) => planet.orbital_period
-const surfaceWater = (planet) => planet.surface_water
-const residents = (planet, _, context) => context.people.getConnections(planet.residents)
-const films = (planet, _, context) => context.film.getConnections(planet.films)
-
 export default {
   Planet: {
-    id,
-    rotationPeriod,
-    orbitalPeriod,
-    surfaceWater,
-    residents,
-    films,
+    id: (planet) => planet.url,
+    rotationPeriod: (planet) => planet.rotation_period,
+    orbitalPeriod: (planet) => planet.orbital_period,
+    surfaceWater: (planet) => planet.surface_water,
+    residents: (planet, _, context) => context.people.getConnections(planet.residents),
+    films: (planet, _, context) => context.film.getConnections(planet.films),
   },
 }
