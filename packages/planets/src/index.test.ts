076e3221e5f99c6c41271fad874033b7fe56075a
Nick Nance <nance.nick@gmail.com>
2017-06-19 21:36:51 -0700

    add test for all planets

--- packages/planets/src/index.test.ts
+++ packages/planets/src/index.test.ts
@@ -40,9 +40,15 @@ describe('Query Execution Tests', () => {
     before(async () => {
         schema = await planetsPlugin(pluginOptions)
     })
-    it('Allows querying for a single plant', async () => {
+    it('Allows querying for a single planet', async () => {
         const results = await graphql(schema, '{ planet(planetID: 1) { name }}')
         expect(results).to.exist
         expect(results.data.planet.name).to.equal('Tatooine')
+    }).timeout(5000)
+    it('Allows querying for a all planets', async () => {
+        const results = await graphql(schema, '{ allPlanets { name }}')
+        expect(results).to.exist
+        expect(results.data.allPlanets.length).to.equal(61)
+        expect(results.data.allPlanets[2].name).to.equal('Hoth')
     }).timeout(5000)
 })
