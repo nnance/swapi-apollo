25232048d2a17856492eaec8a31eae6354f4a59e
Nick Nance <nance.nick@gmail.com>
2017-07-08 18:09:45 -0700

    rename schema package to types

---
+++ packages/types/src/index.test.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import * as mocha from 'mocha'
+import { DocumentNode, ObjectTypeDefinitionNode } from 'graphql'
+import planetsPlugin from './index'
+
+// tslint:disable:no-unused-expression
+describe.only('Loading Types Document Tests', () => {
+    let doc: DocumentNode
+    before(async () => {
+        doc = await planetsPlugin()
+    })
+    it('Document can be loaded', async () => {
+        expect(doc).to.exist
+    })
+    it('Document has a planet type', () => {
+        expect(doc.definitions.find(_ => (_ as ObjectTypeDefinitionNode).name.value === 'Planet')).to.exist
+    })
+})
