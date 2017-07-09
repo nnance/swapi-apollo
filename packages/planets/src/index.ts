a7976cb0a2f859a5131ebcbceed4fbb3181db2c3
Nick Nance <nance.nick@gmail.com>
2017-07-08 19:19:05 -0700

    update planets to types mdoule

--- packages/planets/src/index.ts
+++ packages/planets/src/index.ts
@@ -1,14 +1,30 @@
-import { loadSchema } from '@creditkarma/graphql-loader'
-import { GraphQLSchema } from 'graphql'
+import * as path from 'path'
+import { loadDocument, combineDocuments } from '@creditkarma/graphql-loader'
+import { GraphQLSchema, DocumentNode } from 'graphql'
 import { addResolveFunctionsToSchema } from 'graphql-tools'
+import graphqlTypes from 'swapi-apollo-types'
 import getResolversWithFetchers from './resolvers'
 import { getFetcher, getLoader, IFetcher } from './connectors/swapi'
 
-export default (fetcher: IFetcher): Promise<GraphQLSchema> => {
-    return loadSchema('./schema/*.gql')
-        .then(schema => {
-            const resolvers = getResolversWithFetchers(fetcher)
-            addResolveFunctionsToSchema(schema, resolvers)
-            return schema
-        })
+export interface IGraphQLModule {
+    document: DocumentNode,
+    resolvers: any
 }
+
+export const getSchema = async (fetcher: IFetcher): Promise<GraphQLSchema> => {
+    const gqlModule = await getGraphQLModule(fetcher)
+    const typesDoc = await graphqlTypes()
+    const schema = combineDocuments([typesDoc, gqlModule.document])
+    addResolveFunctionsToSchema(schema, gqlModule.resolvers)
+    return schema
+}
+
+export const getGraphQLModule = (fetcher: IFetcher): Promise<IGraphQLModule> => {
+    return loadDocument(path.resolve(__dirname, '..') + '/graphql/*.gql')
+        .then(document => ({
+            document: document,
+            resolvers: getResolversWithFetchers(fetcher),
+        }))
+}
+
+export default getGraphQLModule
